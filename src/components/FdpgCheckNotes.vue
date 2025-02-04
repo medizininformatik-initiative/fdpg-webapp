247905746e6c4fdce210ee2f34bd9f8a7f44558c
Elahe Karamzadeh Motlagh <elahe.karamzadehmotlagh@appsfactory.de>
2025-02-04 14:06:46 +0100

    [ADD] ZARS-713 make contact and vote section availbale for all

--- src/components/FdpgCheckNotes.vue
+++ src/components/FdpgCheckNotes.vue
@@ -7,9 +7,7 @@
       <FdpgTextEditor v-model="fdpgCheckNotes" :placeholder="$t('proposal.checkNotesPlaceHolder')"></FdpgTextEditor>
       <el-button class="button__margin" type="primary" @click="sendFdpgCheckNotes">{{ $t('general.save') }}</el-button>
     </div>
-    <div class="check-note-content" v-else>
-      {{ proposalStore?.currentProposal?.fdpgCheckNotes }}
-    </div>
+    <div class="check-note-content" v-else v-html="proposalStore.currentProposal.fdpgCheckNotes"></div>
     <el-divider />
   </div>
 </template>
--- src/components/Proposals/Details/DizMemberDetails.vue
+++ src/components/Proposals/Details/DizMemberDetails.vue
@@ -4,6 +4,8 @@
     <QuickInfo :items="quickInfo"></QuickInfo>
     <AppendixInfo></AppendixInfo>
     <ProjectStatus :proposal-status="status"></ProjectStatus>
+    <ContractParticipants v-if="showContractingParticipants" />
+    <LocationVotePanel v-if="showLocationVotePanel" />
     <ProjectTodos :is-disabled="proposalStore.currentProposal?.isLocked" :project-todos="projectTodos"></ProjectTodos>
     <ProjectPublications v-if="showPublications"></ProjectPublications>
     <ProjectHistory />
@@ -44,6 +46,8 @@ import ProjectTodos from '@/components/ProjectTodos.vue'
 import ProjectHistory from '@/components/Proposals/Details/ProjectHistory.vue'
 import QuickInfo from '@/components/QuickInfo.vue'
 import SignDialog from '@/components/SignDialog.vue'
+import ContractParticipants from '@/components/ContractParticipants.vue'
+import LocationVotePanel from '@/components/LocationVotePanel.vue'
 import useNotifications from '@/composables/use-notifications'
 import { useLayoutStore } from '@/stores/layout.store'
 import { useMessageBoxStore, type DecisionType } from '@/stores/messageBox.store'
@@ -68,6 +72,22 @@ const messageBoxStore = useMessageBoxStore()
 const { params } = useRoute()
 const proposalId = computed(() => params.id as string)
 const status = computed(() => proposalStore.currentProposal?.status as ProposalStatus)
+
+const showContractingParticipants = computed(() => {
+  return (
+    status.value === ProposalStatus.Contracting ||
+    status.value === ProposalStatus.ExpectDataDelivery ||
+    status.value === ProposalStatus.DataResearch ||
+    status.value === ProposalStatus.DataCorrupt ||
+    status.value === ProposalStatus.ReadyToArchive ||
+    status.value === ProposalStatus.FinishedProject ||
+    status.value === ProposalStatus.Archived ||
+    status.value === ProposalStatus.Rejected
+  )
+})
+const showLocationVotePanel = computed(() => {
+  return status.value === ProposalStatus.LocationCheck || showContractingParticipants.value
+})
 const currentProposalStatus = [
   ProposalStatus.ExpectDataDelivery,
   ProposalStatus.DataResearch,
@@ -97,6 +117,7 @@ const setDizApproval = async (decision: DizApprovalDecision) => {
 }
 
 const isDeclineDialogOpen = ref(false)
+
 const handleDizDeclineConfirm = async (declineReason: string) => {
   await setDizApproval({ value: false, declineReason })
 }
