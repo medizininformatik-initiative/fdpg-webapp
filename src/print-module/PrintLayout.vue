d345ca4c93bf725ec1771020e9f38473e5dc6b89
Elahe Karamzadeh <elahe.karamzadehmotlagh@appsfactory.de>
2025-02-19 14:08:31 +0100

    [FIX] ZARS-701 Style of the formated texts in exported pdf (#52)

--- src/print-module/PrintLayout.vue
+++ src/print-module/PrintLayout.vue
@@ -5,7 +5,7 @@
 <script setup lang="ts"></script>
 
 <style lang="scss">
-body {
+body:not(.ql-editor) {
   counter-reset: h2 h3 h4;
 }
 
@@ -13,46 +13,46 @@ body {
   margin: 1cm 1cm 1cm 1.5cm;
 }
 
-h1 {
+h1:not(.ql-editor h1) {
   counter-reset: h2;
 }
 
-h2 {
+h2:not(.ql-editor h2) {
   counter-reset: h3;
 }
 
-h3 {
+h3:not(.ql-editor h3) {
   counter-reset: h4;
 }
 
-h2::before {
+h2:not(.ql-editor h2)::before {
   counter-increment: h2;
   content: counter(h2) '. ';
 }
 
-h3::before {
+h3:not(.ql-editor h3)::before {
   counter-increment: h3;
   content: counter(h2) '.' counter(h3) '. ';
 }
 
-h4::before {
+h4:not(.ql-editor h4)::before {
   counter-increment: h4;
   content: counter(h2) '.' counter(h3) '.' counter(h4) '. ';
 }
 
-h2:not(:first-of-type) {
+h2:not(:first-of-type):not(.ql-editor h2) {
   break-before: page;
 }
 
-.print-region {
+.print-region:not(.ql-editor .print-region) {
   break-inside: avoid;
 }
 
-h1,
-h2,
-h3,
-h4,
-h5 {
+h1:not(.ql-editor h1),
+h2:not(.ql-editor h2),
+h3:not(.ql-editor h3),
+h4:not(.ql-editor h4),
+h5:not(.ql-editor h5) {
   span:not(:last-child):after {
     content: ' ';
   }
