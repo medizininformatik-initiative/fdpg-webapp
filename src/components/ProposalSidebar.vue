4beb9de9423b52ee62328d2145e5f09b8c32d682
Elahe Karamzadeh <elahe.karamzadehmotlagh@appsfactory.de>
2025-05-02 12:25:51 +0200

    [FIX] ZARS-700 fix height for proposalSide bar

--- src/components/ProposalSidebar.vue
+++ src/components/ProposalSidebar.vue
@@ -3,7 +3,7 @@
     <img :src="logoSrc" alt="" class="logo" />
     <div class="proposal-menu">
       <div class="proposal-menu__top">
-        <div style="height: calc(100vh - 200px); max-width: 600px">
+        <div style="height: 630px; max-width: 600px">
           <el-steps direction="vertical" :active="activeTab" finish-status="success">
             <el-step
               v-for="step in layoutStore.createProposalSteps"
@@ -77,7 +77,6 @@ const getStepStatus = (step: string): 'success' | 'process' | 'wait' | 'error' =
 
 <style lang="scss">
 @use '@/assets/sass/variable' as *;
-@use 'sass:color';
 
 @mixin sidebar-block {
   width: $sidebar-width !important;
@@ -102,7 +101,6 @@ const getStepStatus = (step: string): 'success' | 'process' | 'wait' | 'error' =
     .el-step__icon {
       background-color: $blue !important;
       color: $white !important;
-      /* border-color: $blue !important; */
     }
   }
   &.is-error {
@@ -120,6 +118,7 @@ const getStepStatus = (step: string): 'success' | 'process' | 'wait' | 'error' =
     }
   }
 }
+
 .el-step.is-vertical {
   padding: 0 16px;
   .el-step__main {
@@ -128,6 +127,7 @@ const getStepStatus = (step: string): 'success' | 'process' | 'wait' | 'error' =
     }
   }
 }
+
 .el-step.is-vertical.is-process {
   background-color: $gray-200;
 }
@@ -136,7 +136,7 @@ const getStepStatus = (step: string): 'success' | 'process' | 'wait' | 'error' =
   height: 100%;
   display: flex;
   position: fixed;
-  padding: 24px 0px;
+  padding: 24px 0;
   background: $white;
   flex-direction: column;
   z-index: $sidebar-z-index;
@@ -151,7 +151,7 @@ const getStepStatus = (step: string): 'success' | 'process' | 'wait' | 'error' =
   }
 
   .proposal-menu {
-    border-top: $gray-100 1px solid;
+    border-top: 1px solid $gray-100;
     width: 100%;
     height: 100%;
     padding-top: 15px;
@@ -165,98 +165,11 @@ const getStepStatus = (step: string): 'success' | 'process' | 'wait' | 'error' =
       color: $gray-900;
       margin-left: 8px;
     }
+
     .step-title {
       font-size: 18px;
       color: $gray-900;
       font-weight: 900;
-    }
-    .proposal-menu__item {
-      height: 44px;
-      margin: 8px 0;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      font-weight: 600;
-      color: $gray-900;
-      line-height: 45px;
-      border-radius: 4px;
-      padding: 0 20px !important;
-      text-decoration-line: none;
-
-      &.proposal-menu__item--exact-active,
-      &:focus,
-      &:hover {
-        background-color: $gray-200;
-      }
-
-      i {
-        color: $gray-900;
-        width: 20px;
-        text-align: center;
-        font-size: 20px;
-        margin-right: 12px;
-        margin-left: 2px;
-      }
-
-      &.proposal-menu__item--exact-active {
-        i {
-          color: $blue;
-        }
-      }
-
-      .proposal-menu__wrapper {
-        display: flex;
-        align-items: center;
-      }
-      .proposal-sidebar__url {
-        color: $gray-900;
-        text-decoration: none;
-      }
-
-      .proposal-menu__icons {
-        float: right;
-
-        .proposal-menu__icon {
-          border-radius: 8px;
-          padding: 0 8px;
-          line-height: 24px;
-          margin-left: 4px;
-          border: 1px solid;
-
-          &--critical {
-            background: $red-100;
-            border-color: $red-100;
-            color: $white;
-          }
-
-          &--high {
-            background: $blue;
-            border-color: $blue;
-            color: $white;
-          }
-
-          &--medium {
-            background: $white;
-            border-color: $white;
-            color: $black;
-          }
-
-          &--low {
-            background: rgba(106, 116, 132, 0.1);
-            border-color: rgba(106, 116, 132, 0.1);
-            color: $gray-900;
-          }
-        }
-      }
-    }
-
-    .el-divider {
-      background-color: $gray-300;
-
-      &.el-divider--horizontal {
-        height: 2px;
-        margin: 23px 0;
-      }
     }
   }
 
