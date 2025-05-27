649db115c1ccdd5a5abfaf88b58bae1232ae5f31
Erwin Gronek <erwin.gronek@appsfactory.de>
2025-05-27 10:22:41 +0200

    ZARS-907 fix wrong researcher displayed

--- src/pages/Proposals/NewPage.vue
+++ src/pages/Proposals/NewPage.vue
@@ -684,6 +684,7 @@ const handleSaveDraft = async () => {
 
 const authStore = useAuthStore()
 const setUpPage = async () => {
+  console.log({ curr: proposalStore.currentProposal })
   proposalForm.value = transformForm(proposalStore.currentProposal, false, authStore.profile) as IProposal
   const lastDashboard = layoutStore.lastDashboard
   layoutStore.setBreadcrumbs([
