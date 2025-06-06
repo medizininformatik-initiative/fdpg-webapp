1d8bd666e1a68c83e654e179368ae89cae23b046
Elahe Karamzadeh <elahe.karamzadehmotlagh@appsfactory.de>
2025-06-06 12:37:54 +0200

    [FIX] ZARS-702 move cohorts to userproject

--- src/pages/Proposals/NewPage.vue
+++ src/pages/Proposals/NewPage.vue
@@ -107,7 +107,7 @@
           />
           <MiiCohortSelection
             v-if="isMIISelected"
-            v-model="proposalForm.cohorts"
+            v-model="proposalForm.userProject.cohorts"
             :review-mode="isReviewMode"
             :feasibilityForm="proposalForm.userProject.feasibility"
             @update:feasibility-form="
@@ -362,7 +362,7 @@ const stepFieldsMap = {
     'requestedData.desiredDataAmount',
   ],
   [CreatPrposalSteps.Casesohort]: [
-    'cohorts',
+    'userProject.cohorts',
     'userProject.feasibility.details',
     'userProject.selectionOfCases.difeSelectionOfCases',
     'requestedData.patientInfo',
@@ -437,18 +437,6 @@ const rules = ref<Record<string, any>>({
   ],
   participants: [
     /** Handled in component */
-  ],
-  cohorts: [
-    {
-      validator: (_rule: any, value: any[], callback: (error?: Error) => void) => {
-        if (!value || value.length === 0) {
-          callback(new Error(t('general.requiredField')))
-        } else {
-          callback()
-        }
-      },
-      trigger: ['blur', 'change'],
-    },
   ],
   projectUser: {
     projectUserType: [requiredValidationFunc('string')],
@@ -545,6 +533,18 @@ const rules = ref<Record<string, any>>({
         otherExplanation: [requiredValidationFunc('string'), maxLengthValidationFunc(10000)],
       },
     },
+    cohorts: [
+      {
+        validator: (_rule: any, value: any[], callback: (error?: Error) => void) => {
+          if (!value || value.length === 0) {
+            callback(new Error(t('general.requiredField')))
+          } else {
+            callback()
+          }
+        },
+        trigger: ['blur', 'change'],
+      },
+    ],
   },
   requestedData: {
     patientInfo: [requiredValidationFunc('string'), maxLengthValidationFunc(10000)],
