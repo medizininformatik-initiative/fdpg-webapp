3a462475ea5b2a50765d71e374dd42552b9c14d6
Elahe Karamzadeh <elahe.karamzadehmotlagh@appsfactory.de>
2025-10-06 11:05:27 +0200

    [FIX] ZARS-676 improve reactivity to show the remove btn immidately (#181)

--- src/components/ParticipatingResearcher.vue
+++ src/components/ParticipatingResearcher.vue
@@ -376,6 +376,10 @@ const handleParticipantTypeSelect = async (participant: ParticipantInfo, newType
         return p
       }) ?? []
     await proposalStore.updateParticipants(proposalId, updatedParticipants)
+
+    researcherIdentities.value = await proposalStore.getResearcherInfo(proposalId)
+    participantsCount.value = researcherIdentities.value.length
+
     showSuccessMessage()
   } catch (error) {
     console.error('Error updating participant type:', error)
@@ -440,6 +444,11 @@ const handleParticipantRoleSelect = async (participant: ParticipantInfo, newRole
       }) ?? []
 
     await proposalStore.updateParticipants(proposalId, updatedParticipants)
+
+    // Refresh researcher identities to get updated data from backend
+    researcherIdentities.value = await proposalStore.getResearcherInfo(proposalId)
+    participantsCount.value = researcherIdentities.value.length
+
     showSuccessMessage()
   } catch (error) {
     console.error('Error updating participant role:', error)
