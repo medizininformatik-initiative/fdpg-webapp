0153d0654c8abd0f4736a030aa04bcc287adf876
Elahe Karamzadeh <elahe.karamzadehmotlagh@appsfactory.de>
2025-12-03 17:20:27 +0100

    [ADD] ZARS-1174 file size restriction added (#223)

--- src/components/ReportDialog.vue
+++ src/components/ReportDialog.vue
@@ -45,6 +45,7 @@
             mode="picture"
             @change="handleChangeFileList"
             @remove="handleRemoveFileList"
+            :max-file-size="5 * 1024 * 1024"
           >
             <el-button
               class="upload-button"
